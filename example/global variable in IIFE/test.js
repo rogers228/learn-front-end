7e9f2213c1026c5fe2b1e75aa738cb284b512b11
rogers Lu <61651257+rogers228@users.noreply.github.com>
2022-06-24 11:39:20 +0800

    Update test.js

--- example/global variable in IIFE/test.js
+++ example/global variable in IIFE/test.js
@@ -1,11 +1,14 @@
 ;(function (){
     console.log('test run1')
-
+    window.a = 125;
 })();
 
 ;(function(){
     console.log('test run2')
-    window.a = 45;
+    let w = window;
+    w.b = 450;
+
 })();
 
 console.log('global a:', a)
+console.log('global b:', b)
