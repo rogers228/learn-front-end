8afa754add9c738c7348be9105522c92e5ef16e3
rogers Lu <61651257+rogers228@users.noreply.github.com>
2022-06-28 18:28:11 +0800

    修改前端路由

--- example/check url/test.js
+++ example/check url/test.js
@@ -1,19 +1,24 @@
-window.addEventListener('hashchange', function () {
-    // console.log('location changed!');
-    console.log(get_url_parameter());
-});
+window.addEventListener('hashchange', router_layout0);
+window.addEventListener("DOMContentLoaded", router_layout0);
 
-window.addEventListener('DOMContentLoaded', function (){
-    console.log('DOMContentLoaded');
-});
+function router_layout0(){
+    //依照網址參數 將layout0排至最前方
+    let parameter = get_url_parameter();
+    if (parameter == ''){
+        return
+    }
+    let walls = document.querySelectorAll('.layout0_wall');
+    walls.forEach((node)=>{
+        let e = document.getElementById(node.id)
+        let router = e.getAttribute('data-router');
+        e.style['z-index'] = "300";
+        if (parameter.indexOf(router) == 0){
+            e.style['z-index'] = "301";
+        }
+    });
+}
 
 function get_url_parameter(){
     let myurl = window.location.href;
-    // if (myurl.includes('#')){
-    //     return window.location.href.split('#').pop()
-    // }
-    // else{
-    //     return ''
-    // }
     return (myurl.includes('#'))? myurl.split('#').pop(): '';
 }
