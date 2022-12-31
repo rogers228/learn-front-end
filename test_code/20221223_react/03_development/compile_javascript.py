6dcbb66cacf54e65af17ce5bdeff7ff39357236a
rogers228 <61651257+rogers228@users.noreply.github.com>
2022-12-31 10:54:36 +0800

    新增

---
+++ test_code/20221223_react/03_development/compile_javascript.py
@@ -0,0 +1,8 @@
+import os
+
+def test1():
+	files = os.listdir(r'C:\Users\USER\Documents\learn-front-end\test_code\20221223_react\15_javascript')
+	print(files)
+
+if __name__ == '__main__':
+	test1()
--- test_code/20221223_react/03_development/development.bat
+++ test_code/20221223_react/03_development/development.bat
@@ -1,3 +1,4 @@
+title development_for_react
 cd /d C:\Users\USER\Documents\learn-front-end\test_code\20221223_react
 set FLASK_APP=app
 set FLASK_ENV=development
