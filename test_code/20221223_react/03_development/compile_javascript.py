8db88a00972f28924e4863e5f8b1dc125d8eeed5
rogers228 <61651257+rogers228@users.noreply.github.com>
2022-12-31 11:15:56 +0800

    新增

--- test_code/20221223_react/03_development/compile_javascript.py
+++ test_code/20221223_react/03_development/compile_javascript.py
@@ -2,7 +2,11 @@ import os
 
 def test1():
 	files = os.listdir(r'C:\Users\USER\Documents\learn-front-end\test_code\20221223_react\15_javascript')
+	lis_comp =[e for e in files if e[:1]=='_']
+	
+
 	print(files)
+	print(lis_comp)
 
 if __name__ == '__main__':
 	test1()
